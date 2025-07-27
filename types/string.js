b16fdb8efe283f704401d8d6f1d9f712035b5f02
jumpeebunee <jumpeebunee@gmail.com>
2025-07-27 20:17:05 +0300

    string

---
+++ types/string.js
@@ -0,0 +1,7 @@
+const LANG = "JS";
+let message = `Lets learn ${LANG}`;
+
+// Ecsape Symbols
+
+let str1 = "Hello \nWorld";
+let str2 = '"Privet, let\'s go"';
---
+++ variables/base.js
@@ -0,0 +1,8 @@
+// console.log(nr1); - ReferenceError
+// console.log(nr2); - undefined
+
+let nr1 = 12;
+var nr2 = 8;
+const PI = 3.14159;
+
+// PI = 12; - TypeError
