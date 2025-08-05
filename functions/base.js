784c3e5f7460fb6d8c2c35e8f461a244b75092b2
jumpeebunee <jumpeebunee@gmail.com>
2025-08-05 21:39:38 +0300

    func

---
+++ functions/base.js
@@ -0,0 +1,12 @@
+// Spread Operator
+function test(x, y, z, a) {
+  console.log(x + y + z + a);
+}
+
+// Rest Param
+function test2(param1, ...param2) {
+  console.log(param1, param2);
+}
+
+test(...[1, 2], ...[3, 4]);
+test2("hi", ["there", "How are you?"]);
